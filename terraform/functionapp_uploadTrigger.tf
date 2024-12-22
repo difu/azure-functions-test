11185a8cbaf23033ce82df742317054b109086ff
Dirk Fuchs <difu@mac.com>
2024-12-22 16:53:32 +0100

    Add build process for BlobTrigger FunctionApp deployment
    Introduced variables and build logic to generate a zip file for the BlobTrigger FunctionApp prior to deployment. Updated storage blob and FunctionApp settings to utilize the generated zip file for deployment. This ensures files are packaged dynamically based on source changes.

--- terraform/functionapp_uploadTrigger.tf
+++ terraform/functionapp_uploadTrigger.tf
@@ -1,10 +1,40 @@
+variable "source_dir_uploadTrigger" {
+  default = "../function/BlobTrigger/"
+}
+
+variable "output_zip_uploadTrigger" {
+  default = "./build/function_uploadTrigger.zip"
+}
+
+resource "null_resource" "build_zip_uploadTrigger" {
+  # Triggers rebuild when files in the source directory change
+  triggers = {
+  checksum = join(
+    "",
+    [for file_path in fileset(var.source_dir_uploadTrigger, "**/*") : md5(file("${var.source_dir_uploadTrigger}/${file_path}"))]
+  )  }
+
+  provisioner "local-exec" {
+    command = <<EOT
+      mkdir -p ./build
+      zip -r -j ${var.output_zip_uploadTrigger} ${var.source_dir_uploadTrigger}
+    EOT
+  }
+  provisioner "local-exec" {
+    when    = destroy
+    command = <<EOT
+      rm -rf ./build
+    EOT
+  }
+}
+
 # Storage Blob Trigger Function Code Deployment
 resource "azurerm_storage_blob" "function_upload_trigger_code_blob" {
   name                   = "function_upload_trigger.zip"
   storage_account_name   = azurerm_storage_account.storageacct.name
   storage_container_name = azurerm_storage_container.function_upload_code.name
   type                   = "Block"
-  source                 = "${path.module}/function_code.zip"
+  source                 = var.output_zip_uploadTrigger
 }
 
 resource "azurerm_function_app" "linuxfunction" {
@@ -24,7 +54,7 @@ resource "azurerm_function_app" "linuxfunction" {
   version                    = "~4"
   app_settings = {
     "AzureWebJobsStorage"      = azurerm_storage_account.storageacct.primary_connection_string
-    "WEBSITE_RUN_FROM_PACKAGE" = "https://${azurerm_storage_account.storageacct.name}.blob.core.windows.net/function-code/${azurerm_storage_blob.function_upload_trigger_code_blob.name}"
+    "WEBSITE_RUN_FROM_PACKAGE" = "https://${azurerm_storage_account.storageacct.name}.blob.core.windows.net/function-upload-code/${azurerm_storage_blob.function_upload_trigger_code_blob.name}"
     "FUNCTIONS_WORKER_RUNTIME" = "python"
     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.ai_function_app.instrumentation_key
 
